7a8f960650216b55efd4854f4c08aa4a7a993b88
niaza712 <157500936+niaza712@users.noreply.github.com>
2024-04-16 14:18:58 -0400

    mm

---
+++ script.js
@@ -0,0 +1,41 @@
+// script.js
+
+function performDivination() {
+    const question = document.getElementById('question').value;
+    const outputDiv = document.getElementById('output');
+    
+    // Clear previous output
+    outputDiv.innerHTML = '';
+  
+    // Define possible responses
+    const responses = [
+      "It is certain",
+      "It is decidedly so",
+      "Without a doubt",
+      "Yes, definitely",
+      "You may rely on it",
+      "As I see it, yes",
+      "Most likely",
+      "Outlook good",
+      "Yes",
+      "Signs point to yes",
+      "Reply hazy try again",
+      "Ask again later",
+      "Better not tell you now",
+      "Cannot predict now",
+      "Concentrate and ask again",
+      "Don't count on it",
+      "My reply is no",
+      "My sources say no",
+      "Outlook not so good",
+      "Very doubtful"
+    ];
+  
+    // Randomly select a response
+    const randomIndex = Math.floor(Math.random() * responses.length);
+    const divinationMessage = responses[randomIndex];
+    
+    // Display divinatory message
+    outputDiv.innerHTML = `<p>${divinationMessage}</p>`;
+  }
+  
